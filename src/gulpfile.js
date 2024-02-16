2b8d6bbebf0034a52f1b490b4996148a6f7af9f7
5420 <olllegborisov@gmail.com>
2024-02-16 14:32:08 +0300

    some paths were changed

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,13 +1,13 @@
-const gulp         = require('gulp');
-const browserSync  = require('browser-sync');
-const sass         = require('gulp-sass')(require('sass'));
-const cleanCSS     = require('gulp-clean-css');
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename       = require("gulp-rename");
-const imagemin     = require('gulp-imagemin');
-const htmlmin      = require('gulp-htmlmin');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
-gulp.task('server', function() {
+gulp.task('server', function () {
 
     browserSync({
         server: {
@@ -18,17 +18,17 @@ gulp.task('server', function() {
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     return gulp.src("src/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest("styles"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch("src/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("pages/*.html").on('change', gulp.parallel('html'));
 });
@@ -45,7 +45,7 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('fonts', function () {
-    return gulp.src("src/fonts/**/*")
+    return gulp.src("src/**/*")
         .pipe(gulp.dest("dist/fonts"));
 });
 
